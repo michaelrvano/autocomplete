a2ad268e8b14baea8ab928948625c37201b363a5
Michael Vano <vano@af13.ca>
2015-12-01 23:46:39 -0500

    Saving for now

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# jQuery Auto Complete function
-A simple basic autocomplete function to use on any text input
+## jQuery Auto Complete function
+A simple autocomplete function to use on any text input. jQuery required. 
+
+
+Example code to initialize the autocomplete:
+$(el).mvAutocomplete(options);
+
+
+#The options that can be passed into the function are as follows:
+
+container_class: The class that will be used on the container element holding the results. Default is "results".
+
+data: An array of string that will be searched through. Default is an empty object.
+
+loading_html: The HTML that will be shown in the results container while waiting for the return of the post data. Default is "Loading...".
+
+post_data: To go together with the url option. Pass an object that will be passed along with the value inputted into the text box. "{query: string}" will be appended to this post_data object. Default is just {query: string}.
+
+result_class: The class that will be used on each individual result. Default is "result".
+
+url: The url to send a post request at, which, will, return the results. Default is an empty string. This takes priority over the "data" option.
+
+
+
+#Sample Code with all Options:
+$(el).mvAutocomplete({
+	container_class: 'special-results',
+	data: ['string', 'another one', 'city name', 'someone name'], //can be variable used here as well
+	loading_html: 'Loading...Spinner',
+	post_data: {
+		name: 'Something',
+		field: 'Another',
+		category: 2
+	},
+	result_class: 'my-result',
+	url: '/api/query'
+});
+
+
+#Other notes:
+- Works only on text inputs. 
+- The url should return a single array of strings. No multi-dimensional arrays. Too complex. :p
+- Can work on multiple text inputs, if for example, if two input elements have the class "ac" and you used that to run the function ( i.e $('.ac').mvAutocomplete(); ). Both of those inputs will have the exact same functionality
+- To have 2 separate text inputs do separate autocomplete searches, each one has to be referenced differently. 
+- This is mainly for my own use, but feel free to grab it and modify it. I don't mind. All open source. Would be nice if you kept my name in the javascript file. :p
--- mv-autocomplete.js
+++ mv-autocomplete.js
@@ -1,5 +1,5 @@
 /*
-* Writing by Michael Vano
+* Written by Michael Vano
 * Front End Developer
 */
 
@@ -13,15 +13,82 @@ url: use this to grab from a url that will return a list of an array or list of 
 
 container_class: class used on the container displaying the results
 
-result_class: 
+result_class: class to be used on each of the result items
 
 =====================*/
 (function ($) {
 	$.fn.mvAutocomplete = function(options) {
-		if 
+		var options = (typeof options !== typeof undefined) ? options : {};
+		if (this.length > 0)
+		{
+			for (i=0; i < this.length; i++)
+			{
+				new Autocomplete($(this[i]), options);
+			}
+		}
 	};
 }(jQuery));
 
 var Autocomplete = function(el, options) {
+	// console.log('Build Object');
+	var _this = this;
+	_this.el = el;
+	_this.data = (typeof options.data !== typeof undefined) ? options.data : {};
+	_this.url = (typeof options.url !== typeof undefined) ? options.url : '';
+	_this.container_class = (typeof options.container_class !== typeof undefined) ? options.container_class : '';
+	_this.result_class = (typeof options.result_class !== typeof undefined) ? options.result_class : '';
+	_this.results = [];
+	_this.post_data = (typeof options.post_data !== typeof undefined) ? options.post_data : {};
+	_this.callback = (typeof options.callback !== typeof undefined && typeof options.callback === 'function') ? options.callback : '';
+	_this.processing;
+	if (!_this.el.is('input[type="text"]')) 
+	{
+		_this.failed = true;
+		console.log('This function needs to be placed on an input text box');
+	}
+	else
+	{
+		_this.el.keyup(function() { _this.search(); });
+	}
+};
 
+Autocomplete.prototype.search = function() {
+	// console.log('Search');
+	var _this = this;
+	var value = _this.el.val();
+	if (value != '')
+	{
+		_this.results = [];
+		if (_this.url != undefined && _this.url != '')
+		{
+			_this.post_data.query = value;
+			if (_this.processing != undefined) { _this.processing.abort(); }
+			_this.processing = $.post(_this.url, _this.post_data, function(data){
+				_this.results = data;
+				_this.showResults();
+			},'JSON');
+		}
+		else if (_this.data != undefined && !$.isEmptyObject(_this.data) && _this.data != '')
+		{
+			if (!$.isEmptyObject(_this.data))
+			{
+				$.each(_this.data, function(k,v) {
+					if (v.indexOf(value) > -1) { _this.results.push(v); }
+				});
+				_this.showResults();
+			}
+		}
+	}
+};
+
+Autocomplete.prototype.showResults = function() {
+	var _this = this;
+	if (_this.results.length > 0)
+	{
+		console.log('Results!!');
+	}
+	else
+	{
+		console.log('No Results');
+	}
 };
